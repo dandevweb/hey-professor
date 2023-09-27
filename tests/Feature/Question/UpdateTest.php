b16f9692c60106be1c5f454c9fe8f2058be6bbce
Danilo Augusto <dandevweb@gmail.com>
2023-09-27 18:22:23 -0300

    Hey-20: it should make sure that only question with status DRAFT can be updated

--- tests/Feature/Question/UpdateTest.php
+++ tests/Feature/Question/UpdateTest.php
@@ -16,3 +16,33 @@ it('should be able  to update a question', function () {
 
     expect($question->fresh()->question)->toBe('This is a valid question?');
 });
+
+
+it('should make sure that only question with status DRAFT can be updated', function () {
+    $user             = User::factory()->create();
+    $questionNotDraft = Question::factory()->for($user, 'createdBy')->create(['draft' => false]);
+    $draftQuestion    = Question::factory()->for($user, 'createdBy')->create(['draft' => true]);
+
+    actingAs($user);
+
+    put(route('question.update', $questionNotDraft))->assertForbidden();
+    put(route('question.update', $draftQuestion), ['question' => 'New Question'])->assertRedirect();
+});
+
+it('should make sure that only person who created the question can update it', function () {
+    $rightUser = User::factory()->create();
+    $wrongUser = User::factory()->create();
+
+    $question = Question::factory()->create([
+        'created_by' => $rightUser->id,
+        'draft'      => true,
+    ]);
+
+    actingAs($wrongUser);
+    put(route('question.update', $question->id))
+        ->assertForbidden();
+
+    actingAs($rightUser);
+    put(route('question.update', $question->id), ['question' => 'New Question'])
+        ->assertRedirect();
+});
