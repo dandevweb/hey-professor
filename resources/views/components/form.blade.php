35de357f2b63898adf30adcc3d89592391be6c31
Danilo Augusto <dandevweb@gmail.com>
2023-10-06 06:34:36 -0300

    hey-24: implementing frontend for the archive/restore options

--- resources/views/components/form.blade.php
+++ resources/views/components/form.blade.php
@@ -1,10 +1,14 @@
-@props(['action', 'post' => null, 'put' => null, 'delete' => null])
+@props(['action', 'post' => null, 'put' => null, 'delete' => null, 'patch' => null])
 
 <form action="{{ $action }}" method="post" {{ $attributes }}>
     @csrf
 
     @if ($put)
         @method('PUT')
+    @endif
+
+    @if ($patch)
+        @method('PATCH')
     @endif
 
     @if ($delete)
